2b96785d5a52a525161bf59fc7ebf8e69790f534
Alister <hi@alisternorris.com>
2018-02-24 01:12:39 +0000

    add image

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ An async/wait utility belt for node and the browser. Like lodash but for async s
 or if your a yarn user...
 
 `yarn add awaiter`
+
+
+[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
--- index.js
+++ index.js
@@ -1,1 +1,117 @@
-module.exports = {}
+
+
+// return null
+exports.silentCatch = (arr) => {
+  // arrProm.map(prom => (prom.catch(e) => null)))
+
+  // return Promise.all(arrProm)
+  //   .then(vals => vals.filter(val => val !== null));
+
+}
+
+
+exports.retry = (times, internal, prom) => {
+  // arrProm.map(prom => (prom.catch(e) => null)))
+
+  // return Promise.all(arrProm)
+  //   .then(vals => vals.filter(val => val !== null));
+
+}
+
+
+
+https://github.com/sindresorhus/p-props
+export.mapObjAll = (obj) => {
+
+}
+
+
+
+
+export.mapObjRace = (obj) => {
+
+}
+
+
+exports.silentCatchAll = (arrProm) => {
+  // arrProm.map(prom => (prom.catch(e) => null)))
+
+  // return Promise.all(arrProm)
+  //   .then(vals => vals.filter(val => val !== null));
+
+}
+
+exports.silentCatchAll = (arrProm) => {
+  // arrProm.map(prom => (prom.catch(e) => null)))
+
+  // return Promise.all(arrProm)
+  //   .then(vals => vals.filter(val => val !== null));
+
+}
+
+
+// give you back the error message
+exports.returnCatchAll = (arrProm) => {
+  arrProm.map(prom => (prom.catch(e) => e)))
+
+  return Promise.all(arrProm);
+
+}
+
+// give you back a null for any 
+exports.nullCatchAll = (arrProm) => {
+  arrProm.map(prom => (prom.catch(e) => e)))
+
+  return Promise.all(arrProm);
+
+}
+
+
+export.limitAll = (limit, arrProm) => {
+
+
+}
+
+
+
+
+//rject indivudal promise after
+export.timeOut = (ms, prom) => {
+
+}
+
+
+
+https://github.com/sindresorhus/p-throttle
+export.throttle = (ms, prom) => {
+
+}
+
+
+// reject promise.all after
+export.timeOutAll => {
+
+}
+
+
+// reject promise.race after
+export.timeOutRace = (ms, arrProm) => {
+
+}
+
+
+export.
+
+http://bluebirdjs.com/docs/api/promise.some.html
+
+
+
+
+
+export.delay = (ms, value) => {
+  new Promise(res => setTimeout(() => res, ms));
+}
+
+
+
+
